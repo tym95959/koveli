ce5c8a9fb538e839a44898d74841eae9854c77ba
tym95959 <tym95959@gmail.com>
2026-08-02 13:35:54 +0500

    Create firebase-config.js

---
+++ script/firebase-config.js
@@ -0,0 +1,14 @@
+// clist.js
+const firebaseConfigCList = {
+  apiKey: "AIzaSyAj9HPVS22uvIGuaNn5etylBTaiBbXap4U",
+  authDomain: "clist-d9050.firebaseapp.com",
+  projectId: "clist-d9050",
+  storageBucket: "clist-d9050.firebasestorage.app",
+  messagingSenderId: "598441885899",
+  appId: "1:598441885899:web:d8a2341ae7993220d75b4e",
+  measurementId: "G-8TJTGQ2P8S"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfigCList);
+console.log("✅ Firebase initialized successfully from clist.js!");
